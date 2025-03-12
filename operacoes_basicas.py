9c4d443cd2cf650ad198160149f7e229f5e057a8
Mateus Prince <mateus.prince@gmail.com>
2025-03-12 10:02:14 -0300

    Update operacoes_basicas.py

--- operacoes_basicas.py
+++ operacoes_basicas.py
@@ -1,11 +1,14 @@
+# entrada de dados
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
+# cálculos matemáticos
 soma = num1 + num2
 subtracao = num1 - num2
 multiplicacao = num1 * num2
 divisao = num1 / num2
 
+#saída de dados
 print(f"Soma: {soma}")
 print(f"Subtração: {subtracao}")
 print(f"Multiplicação: {multiplicacao}")
